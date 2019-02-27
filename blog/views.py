c28ea45daed167e4cdd30b65f4fab4b80a9d6946
calvinkim <calvin.kim@buzzvil.com>
2019-02-27 23:11:27 +0900

    Modified view.py conflict

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-<<<<<<< HEAD
+
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
-=======
->>>>>>> 82d382e78deb599c18670943af25e1e30285735c
